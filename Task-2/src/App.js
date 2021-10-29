56e077c0f6aab9c8408252616b6708945c79a6fc
mohitlalwani23 <83113399+mohitlalwani23@users.noreply.github.com>
2021-10-29 23:07:24 +0530

    Add files via upload

---
+++ Task-2/src/App.js
@@ -0,0 +1,74 @@
+ import React from 'react';
+ import Resturant from "./component/Basics/Resturant.js";
+ 
+ const App = () => {
+   return (
+     <Resturant/>
+   )
+ }
+ 
+ export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+
+// ***** BASIC *****
+
+// // rafce
+
+// // import React from 'react'
+
+// const App = () => {
+//   return (
+//     <>
+//     <MyName/>
+//   <h1>Hello World </h1>
+//    <p>Awesome</p>
+//    </>  
+// );
+// }
+
+// const MyName = () => {
+//   return <h1>Vinod</h1>
+// }
+
+// // const App = () => {
+// //   return (
+// //     <div>
+// //       Welcome 
+// //     </div>
+// //   )
+// // }
+
+// export default App
